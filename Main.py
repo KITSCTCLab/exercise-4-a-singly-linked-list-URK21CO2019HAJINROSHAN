37ccfed66721548cbd1fa3261fd650a95e5ed6c2
URK21CO2019HAJINROSHAN <91783350+URK21CO2019HAJINROSHAN@users.noreply.github.com>
2022-09-06 12:18:04 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@ class Node:
   """
   Initialises the Node with given attributes
   """
-  self.data = data
-  self.next = next
+    self.data = data
+    self.next = next
 class LinkedList:
   def __init__(self):
   """
