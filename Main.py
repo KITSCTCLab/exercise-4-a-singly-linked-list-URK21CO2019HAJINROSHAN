73c503f6ccd2849ca81a91c4206d00b8a19c8d59
URK21CO2019HAJINROSHAN <91783350+URK21CO2019HAJINROSHAN@users.noreply.github.com>
2022-09-06 12:17:16 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ class LinkedList:
   """
   Initialize the head
   """
-  self.head = None
+    self.head = None
  def insert_at_end(self, data):
   new = Node(data, None)
   current = self.head
@@ -19,30 +19,30 @@ class LinkedList:
   else:
    while current.next is not None:
     current = current.next
-    current.next = new
+   current.next = new
  def status(self):
    elements = []
    current = self.head
-  while current:
-    elements.append(current.data)
-    current = current.next
-  print(elements)
+    while current:
+      elements.append(current.data)
+      current = current.next
+    print(elements)
   
 class Solution:
    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list:
- Optional[LinkedList]) -> Optional[LinkedList]:
-   result = self.get_num(first_list) + self.get_num(second_list)
-   sum_list = LinkedList()
- for digit in list(map(int, str(result)[::-1])):
-   sum_list.insert_at_end(digit)
- return sum_list
+    Optional[LinkedList]) -> Optional[LinkedList]:
+    result = self.get_num(first_list) + self.get_num(second_list)
+    sum_list = LinkedList()
+    for digit in list(map(int, str(result)[::-1])):
+      sum_list.insert_at_end(digit)
+    return sum_list
 
- def get_num(self, l: Optional[LinkedList]) -> int:
-  curr = l.head
-  if curr is None:
-    return 0
- num = ""
- while curr is not None:
-   num = str(curr.data) + num
-   curr = curr.next
- return int(num)
+   def get_num(self, l: Optional[LinkedList]) -> int:
+    curr = l.head
+    if curr is None:
+      return 0
+    num = ""
+    while curr is not None:
+      num = str(curr.data) + num
+      curr = curr.next
+    return int(num)
